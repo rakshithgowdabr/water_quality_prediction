7ce0f6e062fc07de4b6729b36482f1220e0183a2
rakshigowdabr <rakshithrakshgowda@gmail.com>
2025-11-12 13:14:14 +0530

    updating readme.md

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask,request,app,jsonify,url_for,render_template
+import numpy as np
+import pandas as pd
+
+app=Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+@app.route('/predict_api',methods=['POST'])
+def predict_api():
+    data=request.json['data']
+    print(data)
